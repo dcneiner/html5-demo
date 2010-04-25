71de487ee7c82da55932b6855b45b39552d656df
Douglas Neiner <doug@pixelgraphics.us>
2010-04-25 09:39:49 -0700

    first commit

---
+++ sample.css
@@ -0,0 +1,49 @@
+div {
+  width: 400px;
+  padding: 40px 50px 70px 50px;
+  position: relative;
+  text-align: center;
+  font-family: helvetica;
+  font-size: 16pt;
+  font-weight: bold;
+  background: white;
+  -webkit-box-shadow: rgba(0,0,0,0.4) 2px 2px 10px;
+  -moz-box-shadow: rgba(0,0,0,0.4) 2px 2px 10px;
+  box-shadow: rgba(0,0,0,0.4) 2px 2px 10px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+  position: fixed;
+  left: 50%;
+  margin-left: -250px;
+  top: 50%;
+  margin-top: -50px;
+}
+
+ul {
+  width: 300px;
+  margin: 0 auto;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  left: 100px;
+  top: 85px;
+}
+
+li {
+  width: 51px;
+  margin-right: 30px;
+  float: left;
+}
+
+li:last-child {
+  margin-right: 0;
+}
+
+li span {
+  display: block;
+  height: 25px;
+  width: 25px;
+  float: left;
+}
